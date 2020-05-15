87cdd7bf5a4ee00436873bc247f43010a26f563e
Marko Lemmetty <marko.lemmetty@jollamobile.com>
2020-05-15 14:33:17 +0300

    Support synchronous copyproj

--- osc/commandline.py
+++ osc/commandline.py
@@ -1963,6 +1963,8 @@ class Osc(cmdln.Cmdln):
 
     @cmdln.option('-b', '--with-binaries', action='store_true',
                         help='copy the built binaries over to avoid a rebuild')
+    @cmdln.option('-n', '--now', action='store_true',
+                  help='copy synchronously (may take some time to complete)')
     @cmdln.option('-H', '--with-history', action='store_true',
                         help='replicate the history of each package.')
     @cmdln.option('-o', '--make-older', action='store_true',
@@ -1980,6 +1982,8 @@ class Osc(cmdln.Cmdln):
         It can not yet be done across buildservice instances
 
         The user must be able to create DESTPRJ
+
+        Normally the copy is done asynchronously
 
         usage:
             osc copyprj SOURCEPRJ DESTPRJ
@@ -2009,6 +2013,7 @@ class Osc(cmdln.Cmdln):
                      withhistory = opts.with_history,
                      makeolder = opts.make_older,
                      resign = opts.re_sign,
+                     now = opts.now,
                      comment = comment)
         print("done cp")
         print(r)
