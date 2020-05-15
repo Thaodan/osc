a45baad61d01221180f90f616fa674d464c1647b
Marko Lemmetty <marko.lemmetty@jollamobile.com>
2020-05-15 14:47:19 +0300

    Add -p to copyprj to enable copying of prjconf

--- osc/commandline.py
+++ osc/commandline.py
@@ -2114,6 +2114,8 @@ class Osc(cmdln.Cmdln):
                         help='replicate the history of each package.')
     @cmdln.option('-o', '--make-older', action='store_true',
                         help='No idea')
+    @cmdln.option('-p', '--prjconf', action='store_true',
+                        help='copy the prjconf also')
     @cmdln.option('-r', '--re-sign', action='store_true',
                         help='re-sign the binaries')
     @cmdln.option('-m', '--message', metavar='TEXT',
@@ -2156,6 +2158,7 @@ class Osc(cmdln.Cmdln):
         r = copy_prj(src_apiurl, src_project, dst_project,
                      withbinaries = opts.with_binaries,
                      withhistory = opts.with_history,
+                     withprjconf = opts.prjconf,
                      makeolder = opts.make_older,
                      resign = opts.re_sign,
                      now = opts.now,
--- osc/core.py
+++ osc/core.py
@@ -6282,8 +6282,10 @@ def copy_pac(
 def copy_prj(src_apiurl, src_project, dst_project,
              withbinaries = False,
              withhistory = False,
+             withprjconf = False,
              makeolder = False,
              resign = False,
+             now = False,
              comment = None):
     """
     Create a copy of a project.
@@ -6297,6 +6299,8 @@ def copy_prj(src_apiurl, src_project, dst_project,
         query['withbinaries'] = '1'
     if withhistory:
         query['withhistory'] = '1'
+    if withprjconf:
+        query['withprjconf'] = '1'
     if makeolder:
         query['makeolder'] = '1'
     if resign:
@@ -6305,6 +6309,7 @@ def copy_prj(src_apiurl, src_project, dst_project,
         query['comment'] = comment
     if now:
         query['nodelay'] = '1'
+
     u = makeurl(src_apiurl, ['source', dst_project], query=query)
     print("copyprj ", u, file=sys.stderr)
     f = http_POST(u)
