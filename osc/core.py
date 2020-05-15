617eea2370ad1e05ed870be1941ed53244b52617
Marko Lemmetty <marko.lemmetty@jollamobile.com>
2020-05-15 14:47:19 +0300

    Add -p to copyprj to enable copying of prjconf

--- osc/core.py
+++ osc/core.py
@@ -3928,8 +3928,10 @@ def copy_pac(
 def copy_prj(src_apiurl, src_project, dst_project,
              withbinaries = False,
              withhistory = False,
+             withprjconf = False,
              makeolder = False,
              resign = False,
+             now = False,
              comment = None):
     """
     Create a copy of a project.
@@ -3943,6 +3945,8 @@ def copy_prj(src_apiurl, src_project, dst_project,
         query['withbinaries'] = '1'
     if withhistory:
         query['withhistory'] = '1'
+    if withprjconf:
+        query['withprjconf'] = '1'
     if makeolder:
         query['makeolder'] = '1'
     if resign:
@@ -3951,6 +3955,7 @@ def copy_prj(src_apiurl, src_project, dst_project,
         query['comment'] = comment
     if now:
         query['nodelay'] = '1'
+
     u = makeurl(src_apiurl, ['source', dst_project], query=query)
     print("copyprj ", u, file=sys.stderr)
     f = http_POST(u)
